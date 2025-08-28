49fd50e58af0e5aacce638aa19a94dc11500b392
Rishan <rbanerjee@islstudent.ch>
2025-08-28 14:40:52 +0200

    In this commit, I added an if statement with all the different conditions to check if the call was from a telemarketer or not.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# You solution goes here
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+number4 = int(input())
+
+if (number1 == 9 or number1 == 8) and (number2 == number3) and (number4 == 9 or number4 == 8):
+    print("ignore")
+else:
+    print("answer")
